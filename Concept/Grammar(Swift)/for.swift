98ed76891ce5a0b24bc20a145cb4dd568b1a4f73
hyunstory <hoon00917@naver.com>
2020-12-30 22:48:36 +0900

    2020년12월30일까지 한것

---
+++ Concept/Grammar(Swift)/for.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+// for문
+
+
+// 자주 쓰는 for문 (범위)
+for i in 0..<9{
+    print(i)
+    // 0부터 8까지 출력
+}
+
+//감소
+for i in (0..<9).reversed(){
+    print(i)
+    // 8부터 0까지 출력
+}
